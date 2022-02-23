d359a942c0c4ad88adf82ae54577e8508d069c29
berkantkaya <berkantkaya19_03@hotmail.com>
2022-02-23 13:16:35 +0300

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,12 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import Wordle from '../components/Wordle/Wordle'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Wordle from "../components/Wordle/Wordle";
 
 const Home: NextPage = () => {
   return (
     <div>
-      <Wordle puzzleword='admin'/>
-     </div>
-  )
-}
+      <Wordle />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
